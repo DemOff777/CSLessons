9baa5031327686eda44296ca4a687329def6da75
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2023-08-02 14:51:06 +0300

    Егоренков_Задание1

---
+++ Егоренков_Задание1/Егоренков_Задание1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Егоренков_Задание1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 255;
+            sbyte s = -128;
+            short sh = -32767;
+            ushort us = 65535;
+            int i = -2147483648;
+            uint ui = 4294967295;
+            float fl = 1.1234567f;
+            char ch = '#';
+            string st = "Строка";
+            bool bl = false;
+        }
+    }
+}
