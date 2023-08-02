2815431ae07dac076bbb69d27220f7e4e22e2db6
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2023-08-02 19:58:36 +0300

    Правка1

--- Егоренков_Задание5/Егоренков_Задание5/Program.cs
+++ Егоренков_Задание5/Егоренков_Задание5/Program.cs
@@ -10,11 +10,13 @@ namespace Егоренков_Задание5
     {
         static void Main(string[] args)
         {
-            string partBody1 = "Правая" , side1 = "Рука";
+            string partBody1 = "Правая";
+            string side1 = "Рука";
 
             Console.WriteLine("Часть тела: " + partBody1 + "; Сторона: " + side1) ;
 
-            string partBody2 = side1, side2 = partBody1;
+            string partBody2 = side1;
+            string side2 = partBody1;
 
             Console.WriteLine ("Часть тела: " + partBody2 + "; Сторона: " + side2);
         }
