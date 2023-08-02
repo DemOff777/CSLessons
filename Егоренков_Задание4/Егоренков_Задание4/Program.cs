cda2c8dbbf12bd719b482b907a1c841bf180f934
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2023-08-02 19:16:02 +0300

    Задание4

---
+++ Егоренков_Задание4/Егоренков_Задание4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Егоренков_Задание4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picRow = 3;
+            int picTotal = 52;
+
+            Console.WriteLine($"Всего рядов {picTotal / picRow}, остаток {picTotal % picRow}");
+        }
+    }
+}
