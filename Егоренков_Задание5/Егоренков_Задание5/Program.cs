b2725730106d8a5ba02aded001017a3add23927d
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2023-08-02 19:39:32 +0300

    Задание5

---
+++ Егоренков_Задание5/Егоренков_Задание5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Егоренков_Задание5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string partBody1 = "Правая" , side1 = "Рука";
+
+            Console.WriteLine("Часть тела: " + partBody1 + "; Сторона: " + side1) ;
+
+            string partBody2 = side1, side2 = partBody1;
+
+            Console.WriteLine ("Часть тела: " + partBody2 + "; Сторона: " + side2);
+        }
+    }
+}
