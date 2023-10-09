fe4b745d2957ad405112e30c04530f012fe572ad
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2023-10-09 14:58:44 +0300

    правка

---
+++ Задание4_3/Задание4_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Convertation(out number);
+
+            Console.WriteLine("Введенное число: " + number);
+        }
+        static int Convertation(out int number)
+        {
+            int userNumber = 0;
+            bool isValueCorrect = false;
+
+            while (isValueCorrect == false)
+            {
+                Console.Write("Введите число: ");
+                isValueCorrect = int.TryParse(Console.ReadLine(), out userNumber);
+            }
+
+            number = userNumber;
+            return number;
+        }
+    }
+}
