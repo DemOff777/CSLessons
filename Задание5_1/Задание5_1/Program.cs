34cb2f36f55cb939a8246b427234ba0f99500641
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2023-10-17 16:12:37 +0300

    новое

---
+++ Задание5_1/Задание5_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cerukalName = "церукал";
+            string aspirinName = "аспирин";
+            string loperamidName = "лоперамид";
+            string cerukalDescription = "средство от тошноты";
+            string aspirinDescription = "средство от болей";
+            string loperamidDescription = "средство от диареи";
+            string userInput = string.Empty;
+
+            bool isDrugNameCorrect = true;
+
+            Dictionary<string, string> medicines = new Dictionary<string, string>();
+
+            medicines = AddDrug(medicines, cerukalName, cerukalDescription);
+            medicines = AddDrug(medicines, aspirinName, aspirinDescription);
+            medicines = AddDrug(medicines, loperamidName, loperamidDescription);
+
+            while (isDrugNameCorrect)
+            {
+                Console.WriteLine("Введите название препарата: ");
+                userInput = Console.ReadLine();
+
+                if (isDrugNameCorrect == medicines.ContainsKey(userInput))
+                {
+                    Console.WriteLine(medicines[userInput]);
+                }
+                else
+                {
+                    isDrugNameCorrect = false;
+                    Console.WriteLine("Такого препарата нет");
+                }
+            }
+        }
+
+        static Dictionary<string, string> AddDrug(Dictionary<string, string> medicines, string drug, string description)
+        {
+            medicines.Add(drug, description);
+            return medicines;
+        }
+    }
+}
---
+++ Задание5_2/Задание5_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int customersAmount = 5;
+            int productsMinAmount = 1;
+            int productsMaxAmount = 10;
+            int productMinPrice = 10;
+            int productMaxPrice = 500;
+            int cashBox = 0;
+            int randomIndex;
+
+            Random random = new Random();
+
+            Console.Write("Начать покупки");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            for (int i = 0; i < customersAmount; i++)
+            {
+                randomIndex = random.Next(productsMinAmount, productsMaxAmount + 1);
+
+                Queue<int> customerBuys = new Queue<int>();
+
+                for (int j = 0; j < randomIndex; j++)
+                {
+                    customerBuys.Enqueue(random.Next(productMinPrice, productMaxPrice + 1));
+                }
+
+                while (customerBuys.Count != 0)
+                {
+                    Console.WriteLine($"Покупка {customerBuys.Peek()} рублей");
+                    cashBox += customerBuys.Dequeue();
+                }
+         
+                Console.WriteLine($"\nВ кассе: {cashBox} рублей");
+                Console.WriteLine("Товары закончились, нажмите любую клавишу чтобы перейти к следующему покупателю");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
+ 
