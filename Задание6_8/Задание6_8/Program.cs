b59d477530316fe65301c7db1aad7a7d5cbf42af
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2023-11-26 16:32:21 +0300

    правка

--- Задание6_8/Задание6_8/Program.cs
+++ Задание6_8/Задание6_8/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+
 
 namespace Задание6_8
 {
