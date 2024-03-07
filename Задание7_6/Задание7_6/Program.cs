43597bf1a67fcbaecd4227512d4c6812a0b75677
DemOff777 <141236358+DemOff777@users.noreply.github.com>
2024-03-07 15:38:55 +0300

    новое

---
+++ Задание7_6/Задание7_6/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Задание7_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Barracks barracks = new Barracks();
+
+            barracks.ShowSoldiers();
+            Console.WriteLine();
+            Console.WriteLine("Отсортированные солдаты");
+            var sortedSoldiers = barracks.SortSoldiers();
+            barracks.ShowSortedSoldiers(sortedSoldiers);
+        }
+    }
+
+    static class UserUtils
+    {
+        private static Random s_random = new Random();
+
+        public static int GeneradeRandomNumber(int minValue, int maxValue)
+        {
+            return s_random.Next(minValue, maxValue + 1);
+        }
+
+        public static int GeneradeRandomNumber(int maxValue)
+        {
+            return s_random.Next(maxValue);
+        }
+    }
+
+    class Barracks
+    {
+        private List<Soldier> _soldiers = new List<Soldier>();
+
+        public Barracks()
+        {
+            GeneradeSoldiers();
+        }
+
+        public void ShowSortedSoldiers(IEnumerable<> soldiersSorted)
+        {
+            foreach (var soldier in soldiersSorted)
+            {
+                Console.WriteLine($"{soldier.Name} {soldier.Rank}");
+            }
+        }
+
+        public void ShowSoldiers()
+        {
+            foreach (var soldier in _soldiers)
+            {
+                soldier.ShowStats();
+            }
+        }
+
+        public IEnumerable<> SortSoldiers()
+        {
+            return _soldiers.Select(soldier => new { soldier.Name, soldier.Rank });
+        }
+
+        private void GeneradeSoldiers()
+        {
+            int soldiesCount = 20;
+
+            for (int i = 0; i < soldiesCount; i++)
+            {
+                _soldiers.Add(new Soldier());
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public Soldier()
+        {
+            string[] names = { "Petya", "Vasya", "Kolya", "Gerasim", "Luka", "Kostya", "Igor", "Ivan" };
+            string[] weapons = { "Pistol", "MachineGun", "RocketLauncher", "Riffle", "GrenadeLauncher", "ShotGun" };
+            string[] ranks = { "Private", "Sergant", "Foreman", "Lieutenant", "Captain", "General" };
+
+            Name = names[UserUtils.GeneradeRandomNumber(names.Length)];
+            Weapon = weapons[UserUtils.GeneradeRandomNumber(weapons.Length)];
+            Rank = ranks[UserUtils.GeneradeRandomNumber(ranks.Length)];
+
+            GeneradeServiceTime();
+        }
+
+        public string Name { get; private set; }
+
+        public string Weapon { get; private set; }
+
+        public string Rank { get; private set; }
+
+        public int ServiceTime { get; private set; }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"{Name} имеет вооружение {Weapon}, в звании {Rank}, Срок службы {ServiceTime} месяцев");
+        }
+
+        private void GeneradeServiceTime()
+        {
+            int minServiceTimeValue = 12;
+            int maxServiceTimeValue = 50;
+
+            ServiceTime = UserUtils.GeneradeRandomNumber(minServiceTimeValue, maxServiceTimeValue);
+        }
+    }
+}
